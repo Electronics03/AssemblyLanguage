7f8f7d85e07f28b2f2ca5e60601b557400de8d98
Electronics03 <funfunpsh@gmail.com>
2025-04-10 19:24:27 +0900

    컴파일러님 감사합니다

--- BubbleSort/Bsort.s
+++ BubbleSort/Bsort.s
@@ -1,49 +1,49 @@
     .data
-A:  .word 9, 3, 5, 6, 8, 2, 1, 0, 4, 7   # int A[10]
+A:  .word 9, 3, 5, 6, 8, 2, 1, 0, 4, 7
 
     .text
     .globl main
 main:
-    la x10, A          # a0 = 주소 A
-    li x11, 10         # a1 = 배열 길이 10
-    jal ra, sort       # sort(A, 10)
-    li a7, 10          # 종료 syscall
+    la x10, A
+    li x11, 10
+    jal x1, sort
+    li a7, 10
     ecall
 
-swap: 
+swap:
     slli x6, x11, 2
     add x6, x10, x6
     lw x5, 0(x6)
     lw x7, 4(x6)
     sw x7, 0(x6)
     sw x5, 4(x6)
-    jalr x0, 0(ra)
+    jalr x0, 0(x1)
 
 sort:
-        addi sp, sp, -4
-        sw ra, 0(sp)
-        addi x21, x10, 0    # &v -> 21
-        addi x22, x11, 0    # n -> x22
-        addi x19, x0, 0     # i -> x19 = 0
-    for1tst:
-        bge x19, x22, exit1 # if x19(i) >= x22(n) then exit1
-        addi x20, x19, -1   # j -> x20(j) = x19(i) - 1
-    for2tst:
-        blt x20, x0, exit2  # if x20(j) < x0(0) then exit2
-        slli x5, x20, 2     # x5 <- x20(j) * 4
-        add x5, x21, x5     # x5 <- x21(&v) + x20([j]) -> v[j]
-        lw x6, 0(x5)        # x6 <- v[j]
-        lw x7, 4(x5)        # x6 <- v[j+1]
-        ble x6, x7, exit2   # if x6 <= x7 then exit2
-        addi x10, x21, 0    # &v -> 21
-        addi x11, x20, 0    # n -> x22
-        jal ra, swap        # jump to swap
-        addi x20, x20, -1   # j--
-        jal x0, for2tst     # jump to for2tst
-    exit2:
-        addi x19, x19, 1    # i++
-        jal x0, for1tst     # jump to for1tst
-    exit1:
-        lw ra, 0(sp)
-        addi sp, sp, 4
-        jalr x0, 0(ra)
+    addi sp, sp, -4
+    sw x1, 0(sp)
+    addi x21, x10, 0
+    addi x22, x11, 0
+    addi x19, x0, 0
+for1tst:
+    bge x19, x22, exit1
+    addi x20, x19, -1
+for2tst:
+    blt x20, x0, exit2
+    slli x5, x20, 2
+    add x5, x21, x5
+    lw x6, 0(x5)
+    lw x7, 4(x5)
+    ble x6, x7, exit2
+    addi x10, x21, 0
+    addi x11, x20, 0
+    jal x1, swap
+    addi x20, x20, -1
+    jal x0, for2tst
+exit2:
+    addi x19, x19, 1
+    jal x0, for1tst
+exit1:
+    lw x1, 0(sp)
+    addi sp, sp, 4
+    jalr x0, 0(x1)
