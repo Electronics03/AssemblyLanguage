4de948032cceefcf5a562594c7e44d6a98eb465d
Electronics03 <funfunpsh@gmail.com>
2025-04-10 19:25:11 +0900

    C코드 수정

--- BubbleSort/Bsort.c
+++ BubbleSort/Bsort.c
@@ -1,5 +1,4 @@
 #include <stddef.h>
-#include <stdio.h>
 
 void swap(int v[], size_t k)
 {
@@ -16,18 +15,9 @@ void sort(int v[], size_t n)
             swap(v, j);
     return;
 }
-void printArr(int v[], size_t n)
-{
-    for (int i = 0; i < n; i++)
-        printf("%d ", v[i]);
-    printf("\n");
-    return;
-}
 int main(void)
 {
     int A[10] = {9, 3, 5, 6, 8, 2, 1, 0, 4, 7};
-    printArr(A, 10);
     sort(A, 10);
-    printArr(A, 10);
     return 0;
 }
