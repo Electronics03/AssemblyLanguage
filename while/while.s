6cafd239ab575f501e43831bcc0d20d816267c28
Electronics03 <funfunpsh@gmail.com>
2025-04-10 19:29:12 +0900

    md 파일 제거

--- while/while.s
+++ while/while.s
@@ -2,9 +2,9 @@ main:
     li x5, 0
     li x6, 10
 loop:
-    bge x5, x6, end   
-    addi x5, x5, 1    
-    beq x0, x0, loop  
+    bge x5, x6, end
+    addi x5, x5, 1
+    beq x0, x0, loop
 end:
-    li x10, 0         
-    ret               
+    li x10, 0
+    ret
